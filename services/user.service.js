4f9f8b7eddc0a6338e6ea01f1ac490dfc4a766cb
Abdullah KUŞ <abdullahkus38@hotmail.com>
2023-02-27 02:40:26 +0300

    fix: Fixed logic error of "getUsers" method

--- services/user.service.js
+++ services/user.service.js
@@ -1,21 +1,12 @@
 import axios from '@axios';
 
 export const getUsers = async (since = 0, perPage = 100) => {
-    if (!perPage) {
-        return axios.get('/users', {
-            params: {
-                per_page: perPage,
-                since,
-            },
-        });
-    } else {
-        return axios.get('/users', {
-            params: {
-                per_page: perPage,
-                since,
-            },
-        });
-    }
+    return axios.get('/users', {
+        params: {
+            per_page: perPage,
+            since,
+        },
+    });
 };
 
 export const getUser = async (username) => {
