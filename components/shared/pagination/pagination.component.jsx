99838dd0547f2eeec3d4e2e308359e7f0885cc05
Abdullah KUŞ <abdullahkus38@hotmail.com>
2023-02-27 16:02:34 +0300

    fix(pagination): capitalization error

--- components/shared/pagination/pagination.component.jsx
+++ components/shared/pagination/pagination.component.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Link from 'next/link';
-import * as styles from './Pagination.module.css';
+import * as styles from './pagination.module.css';
 
 function Pagination({ currentPage, totalPages }) {
     const pages = [];
