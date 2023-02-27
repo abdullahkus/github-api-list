c87a2fe78d19e2835bd170e98d1e6edd614637ee
Abdullah KUŞ <abdullahkus38@hotmail.com>
2023-02-27 15:54:39 +0300

    chore: purge removed from tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,13 +8,6 @@ module.exports = {
         './containers/**/*.{js,ts,jsx,tsx}',
         './layouts/**/*.{js,ts,jsx,tsx}',
     ],
-    purge: [
-        './app/**/*.{js,ts,jsx,tsx}',
-        './pages/**/*.{js,ts,jsx,tsx}',
-        './components/**/*.{js,ts,jsx,tsx}',
-        './containers/**/*.{js,ts,jsx,tsx}',
-        './layouts/**/*.{js,ts,jsx,tsx}',
-    ],
     theme: {
         extend: {
             colors: {
