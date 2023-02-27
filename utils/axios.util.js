48cd82efc2f146e2543d5742b09184c8805721c4
Abdullah KUŞ <abdullahkus38@hotmail.com>
2023-02-27 16:44:30 +0300

    chore: removed env in next config

--- utils/axios.util.js
+++ utils/axios.util.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const axiosInstance = axios.create({
     baseURL: 'https://api.github.com/',
     headers: {
-        Authorization: 'Bearer ' + process.env.GITHUB_KEY,
+        Authorization: 'Bearer ' + process.env.NEXT_PUBLIC_GITHUB_KEY,
     },
 });
 
